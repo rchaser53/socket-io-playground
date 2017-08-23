2abd4496ca0de58380c26b90eb9937c6cc6b1647
rchaser53 <tayoshizawa29@gmail.com>
2017-08-23 23:40:55 +0900

    interruput ws event and watch event(sync promise) before setTimeout

--- src/index.js
+++ src/index.js
@@ -7,20 +7,41 @@ const vue = new Vue({
   el: '#app',
   data: function() {
     return {
-      count: 0
+      count: 0,
+      flag: false
     }
   },
   template: `<div>
     <button v-on:click="sendMessage">fire</button>
+    <button v-on:click="toggleFlag">toggle</button>
     <div>{{count}}</div>
+    <div style="display:block; width: 100px; height: 100px; background-color: black;" v-if="flag"></div>
   </div>`,
   methods: {
     sendMessage() {
       socket.emit('fireAll', this.count)
+    },
+    toggleFlag() {
+      Vue.set(this, 'flag', !this.flag)
+    }
+  },
+  watch: {
+    flag(newValue) {
+      const startTime = Date.now();
+      while (Date.now() < (startTime + 5000)) {}
+      setTimeout(() => {
+        console.log('setTimeout')
+      }, 0)
+      Vue.set(this, 'count', ++this.count)
+      console.log('flag')
+    },
+    count(newValue) {
+      console.log('count')
     }
   }
 })
 
 socket.on('toClient', (data) => {
-  Vue.set(vue, 'count', data);
+  console.log('toClient');
+  Vue.set(vue, 'count', Math.random() * 1000);
 })
