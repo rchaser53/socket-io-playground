20063ec356548bfa6a6be981e594099da928d443
rchaser53 <tayoshizawa29@gmail.com>
2017-08-27 14:09:22 +0900

    create imitate modal

---
+++ src/imitateModal.js
@@ -0,0 +1,80 @@
+import Vue from 'vue'
+import * as SocketIO from "socket.io-client"
+
+const socket = SocketIO('http://localhost:3000');
+
+const ImitateModal = Vue.extend({
+  props: {
+    id: {},
+    shown: {},
+    open: {},
+    close: {}
+  },
+  data: function() {
+    return {
+      transition: false,
+      outer: null,
+    }
+  },
+  mounted() {
+    Vue.set(this, 'outer', document.querySelector(`#${this.id}`));
+  },
+  template: `<div>
+              <div v-bind:id="id" class="fade" @transitionend="onTransitionEnd">
+                <button v-on:click="sendMessage">fire</button>
+                <button v-on:click="open">open</button>
+                <button v-on:click="close">close</button>
+              </div>
+            </div>`,
+  methods: {
+    sendMessage() {
+      socket.emit('fireAll', 0)
+    },
+    addIn() {
+      this.outer.className = "fade in";
+    },
+    removeIn() {
+      this.outer.className = "fade";
+    },
+    onTransitionEnd() {
+      console.log(123)
+      Vue.set(this, 'transition', false);
+    },
+    initializeModalDialog: function() {
+      Vue.set(this, 'transition', false);
+    },
+  },
+
+  watch: {
+    transition: function (newValue, oldValue) {
+      if (newValue === oldValue) return
+      const self = this;
+
+      if (newValue) {
+          if (this.shown) {
+              setTimeout(function() {
+                  self.addIn();
+              }, 0)
+          } else {
+              self.removeIn();
+          }
+      }
+      else {
+      //     if (!this.shown) {
+      //         // el.style.display = 'none'
+          // }
+      }
+    },
+    shown(newValue, oldValue) {
+      console.log(newValue, oldValue, 111)
+      if(newValue) {
+        this.initializeModalDialog()
+      }
+      if (oldValue === null ? newValue === true : true) {
+          Vue.set(this, 'transition', true)
+      }
+    }
+  }
+});
+
+export default ImitateModal;
--- src/index.js
+++ src/index.js
@@ -1,116 +1,63 @@
 import Vue from 'vue'
 import * as SocketIO from "socket.io-client"
 
+import ImitateModal from './imitateModal';
+
 const socket = SocketIO('http://localhost:3000');
 
 const vue = new Vue({
   el: '#app',
   data: function() {
     return {
-      count: 0,
-      transition: false,
-      shown: false
+      modalA: {
+        shown: false
+      },
+      modalB: {
+        shown: false
+      }
     }
   },
-  template: `<div id="outer" class="fade" @transitionend="onTransitionEnd">
-              <div>
-                <button v-on:click="sendMessage">fire</button>
-                <button v-on:click="openDialog">open</button>
-                <button v-on:click="closeDialog">close</button>
-                <div id="block" style="display:none; width: 100px; height: 100px; background-color: black;"></div>
-              </div>
+  components: {
+    "modal-a": ImitateModal,
+    "modal-b": ImitateModal
+  },
+  template: `<div>
+              <modal-a id="modalA" v-bind:shown="modalA.shown"
+                      v-bind:open="openDialogA" v-bind:close="closeDialogA" />
+              <modal-b id="modalB" v-bind:shown="modalB.shown"
+                      v-bind:open="openDialogB" v-bind:close="closeDialogB" />
             </div>`,
   methods: {
-    sendMessage() {
-      socket.emit('fireAll', this.count)
+    openDialogA() {
+      Vue.set(this.modalA, 'shown', true);
     },
-    showBlock() {
-      document.querySelector('#block').style.display = "block";
+    closeDialogA() {
+      Vue.set(this.modalA, 'shown', false);
     },
-    hideBlock() {
-      document.querySelector('#block').style.display = "none";
+    openDialogB() {
+      Vue.set(this.modalB, 'shown', true);
     },
-    addIn(el) {
-      el.className = "fade in";
-    },
-    removeIn(el) {
-      el.className = "fade";
-    },
-    openDialog() {
-      Vue.set(this, 'shown', true);
-    },
-    closeDialog() {
-      Vue.set(this, 'shown', false);
-    },
-    onTransitionEnd() {
-      console.log(123)
-      Vue.set(this, 'transition', false);
-    },
-    initializeModalDialog: function() {
-      // const el = this.$el
-      // el.classList.remove('in')
-      // el.style.display = 'none'
-      // this.hideBlock();
-      Vue.set(this, 'transition', false);
-    },
-  },
-  watch: {
-    flag(newValue) {
-      const startTime = Date.now();
-      // while (Date.now() < (startTime + 5000)) {}
-      setTimeout(this.showBlock, 0)
-      // Vue.set(this, 'count', ++this.count)
-      this.hideBlock();
-      // console.log('flag')
-    },
-    // count(newValue) {
-    //   this.hideBlock();
-    //   console.log('count')
-    // },
-
-    transition: function (newValue, oldValue) {
-      if (newValue === oldValue) return
-      // // const el = this.$el
-      const self = this;
-      const el = document.querySelector('#outer');
-
-      if (newValue) {
-          if (this.shown) {
-      //         // el.querySelector('.modal-content').focus()
-      //         // el.style.display = 'block'
-              setTimeout(function() {
-      //             // el.classList.add('in')
-                  self.addIn(el);
-              }, 0)
-          } else {
-      //         // el.classList.remove('in')
-              self.removeIn(el);
-          }
-      }
-
-      else {
-      //     if (!this.shown) {
-      //         // el.style.display = 'none'
-          // }
-      }
-    },
-    shown(newValue, oldValue) {
-      if(newValue) {
-        this.initializeModalDialog()
-      }
-      if (oldValue === null ? newValue === true : newValue !== oldValue) {
-          Vue.set(this, 'transition', true)
-      }
+    closeDialogB() {
+      Vue.set(this.modalB, 'shown', false);
     }
-  }
+  },
 })
 
 socket.on('toClient', (data) => {
-  console.log('toClient');
-  Vue.set(vue, 'shown', false);
-  Vue.set(vue, 'shown', true);
+  Vue.set(vue.modalA, 'shown', true);
+  Vue.set(vue.modalA, 'shown', true);
 })
 
+
+  // watch: {
+    // flag(newValue) {
+    //   const startTime = Date.now();
+    //   // while (Date.now() < (startTime + 5000)) {}
+    //   setTimeout(this.showBlock, 0)
+    //   // Vue.set(this, 'count', ++this.count)
+    //   this.hideBlock();
+    // },
+  // }
 
 // var bidCallback = function (error, data) {
 //   vue.closeAllDialogs();
